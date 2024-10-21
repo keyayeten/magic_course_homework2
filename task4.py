5eb53e672acaa022fdb2d8342fa24c033c45f88e
Aleksei Dubrovskii <aleksei.dubrovskii@stm-labs.ru>
2024-10-21 21:23:27 +0300

    ++

--- task4.py
+++ task4.py
@@ -3,35 +3,10 @@
 # а значение - список элементов данного типа.
 # Для списка: [1, 2, "3", "4", True, 5.5]
 # Ответ:  {int: [1, 2, 5], float: [5.5], str: ["3", "4"], bool: [True]}
-
-# решение 1
 elements = [1, 2, "3", "4", True, 5.5]
-type_list = {
-    'int': [],
-    'float': [],
-    'str': [],
-    'bool': []
-}
+type_list = {}    
 for i in elements:
-    if type(i) is int:
-        type_list['int'].append(i)
-    elif type(i) is float:
-        type_list['float'].append(i)
-    elif type(i) is str:
-        type_list['str'].append(i)
-    elif type(i) is bool:
-        type_list['bool'].append(i)
-print(type_list)
-
-# решение 2
-
-elements = [1, 2, "3", "4", True, 5.5]
-type_list = {
-    int: [],
-    float: [],
-    str: [],
-    bool: []
-}
-for i in elements:
+    if type(i) not in type_list:
+        type_list[type(i)] = []
     type_list[type(i)].append(i)
 print(type_list)
