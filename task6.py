386ea733c68cfadb99350130a1856750d870894b
Aleksei Dubrovskii <aleksei.dubrovskii@stm-labs.ru>
2024-10-21 20:57:16 +0300

    ++

--- task6.py
+++ task6.py
@@ -4,7 +4,11 @@
 #  Пример:
 # [1.1, 1.2, 3.1, 5, 10.01] => 0.19
 spk = [1.1, 1.2, 3.1, 5, 10.01]
-num_min = min(spk)
-num_max = max(spk)
-res = abs((num_min - int(num_min)) - (num_max - int(num_max)))
+new_spk = []
+for i in spk:
+    if i - int(i) != 0:
+        new_spk.append((i - int(i)))
+num_min = min(new_spk)
+num_max = max(new_spk)
+res = abs((num_max - int(num_max)) - (num_min - int(num_min)))
 print(f'{res:.2f}')
